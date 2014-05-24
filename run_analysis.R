cb2f001d6c93fc98fd3ca26c56eb87a4f14e07c3
Chris Brown <chriseisbrown@gmail.com>
2014-05-24 17:57:47 +0100

    make col names more friendly

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,12 @@
 # get column names for main data set
 col_name_data <- read.table("UCI HAR Dataset/features.txt", header=FALSE, col.names=c("col_id", "col_name"))
 
+# make the column names a little more user friendly
+col_name_data$new_col_name <- apply(col_name_data, 1, function(x) {  gsub("[[:punct:]]", "", x["col_name"]) } )
+col_name_data$new_col_name <- apply(col_name_data, 1, function(x) {  result <- gsub("mean", "Mean", x["new_col_name"]) } )
+col_name_data$new_col_name <- apply(col_name_data, 1, function(x) {  result <- gsub("std", "Std", x["new_col_name"]) } )
+
+
 # get a vector of the column names
 col_names <- col_name_data[["col_name"]]    
 
@@ -23,13 +29,10 @@ training_data_and_activity_name <- merge(activity_labels, training_data_and_acti
 cols <- colnames(training_data_and_activity_name)
 column_std <- column_finder(cols, "std\\.")
 column_mean <- column_finder(cols, "mean\\.")
-column_std_and_mean <- c(column_std, column_mean)
+# just the columns for std and means with the activity names
+column_std_and_mean <- c("activity_name", column_std, column_mean)
 
-column_std_and_mean
-head(training_data_and_activity_name[, column_std_and_mean)])
-head(training_data_and_activity_name[, c("tBodyAcc.mean...X")])
+# get a subset of the data with these columns
+training_data_and_activity_name_std_mean <- training_data_and_activity_name[, column_std_and_mean]
 
-head(training_data_and_activity_name[, c("activity_id")])
 
-colnames(training_data_and_activity_name)
-cols <- colnames(training_data_and_activity_name)
